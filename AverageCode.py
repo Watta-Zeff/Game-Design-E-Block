427b9b61f11820ce8f7c50c32fc549d923217086
Zeffaniah Knight <sadlerz24@greenhill.org>
2022-01-14 10:00:13 -0600

    Today's Class 1/14

---
+++ AverageCode.py
@@ -0,0 +1,26 @@
+import os
+# Zeffaniah Sadler-Knight
+# Jan 14, 22
+#Declare variables,print variables, print type of data
+#Learn some operators
+
+#Program is Average of 3 tests
+
+os.system('cls')
+#Declare and assign values
+test1=80
+test2=76
+test3=92
+Flag=False
+#to display things on the screen we use the function print
+
+# print(type(test1), type(test2), type(Flag))
+
+#declare sum to add tests symbol for addition is +
+Sum = test1 + test2 + test3
+#Average willus division ..... /
+Average= Sum/3
+print('The Sum is',Sum)
+print('The average of 3 tests is', Average)
+
+#I want to print The average of 3 tests is "number here"
