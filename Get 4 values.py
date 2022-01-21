94f61eb1a5207704dcc3030c03fe198e0e2ad5c4
Watta-Zeff& C:/Users/sadlerz24/AppData/Local/Microsoft/WindowsApps/python3.10.exe c:/Users/sadlerz24/.vscode/Game-Design-E-Block/AverageCode.py <sadlerz24@greenhill.org>
2022-01-20 19:55:53 -0600

    ProgramWriting HW 1/20

---
+++ Get 4 values.py
@@ -0,0 +1,42 @@
+#Zeffaniah Knight
+#01/19/2022
+
+#Objectives Other operators  =-*/  **Exponent   %modulus
+#                   Format Printing escape character
+
+#Progrm creating an equation, aasking user input,
+#  and formatting the output
+
+import os 
+
+
+os.system('cls')
+
+#Declare Variable:
+Var1=4
+Var2=5
+Var3=6
+Var4=2
+Flag=False
+
+#Calculate equation given
+
+result= 3*Var1 - 2*(Var2)**2
+
+
+
+#print(2**4)
+
+equation=((3*Var1)-2*(Var2)**2/(Var3))/Var4
+
+print("The result is = %6.3f"% equation, end=" ----")
+
+print("\n The \" quotes\" tabs \t backslash \\")
+print("Var1= %5d"% Var1)
+print("Var2= %5d"% Var2)
+print("Var3= %5d"% Var3)
+print("Var4= %5d"% Var4)
+
+#Get 4 values 3*V1-2(V2)^2/V3/V4
+
+
---
+++ ProgramWriting.py
@@ -0,0 +1,19 @@
+import os
+
+os.system('cls')
+
+print("Greenhill : 10, Portal : 5.33")
+
+print("Total students : 240, Boys : 120")
+
+print("031")
+
+print("3.561E+02")
+
+print("Welcome to")
+
+print("Greenhill School")
+
+print("I Love    Greenhill")
+
+# print("The string after using repr() is :'I\nLove\tGreenhill'")
