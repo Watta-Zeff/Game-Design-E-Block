a218f01dc9d8d22a6c2d37d0de559ad20c768464
Watta-Zeff& C:/Users/sadlerz24/AppData/Local/Microsoft/WindowsApps/python3.10.exe c:/Users/sadlerz24/.vscode/Game-Design-E-Block/AverageCode.py <sadlerz24@greenhill.org>
2022-01-21 09:59:04 -0600

    First Game

---
+++ userinput.py
@@ -0,0 +1,40 @@
+#Zeffaniah Sadler-knight
+#1/21/22
+
+#We are going to learn the input() function
+# type casting branching. looping
+
+#declare variable for user inout
+
+import os, random
+os.system('cls')
+
+
+# print("enter a number from 1-10:",end=" ")
+
+#  #input returns a string5
+
+
+# useless=int(input())
+# print("the number is %.2f " %(useless/3))
+
+# guess=int(input("please give a number"))
+
+#guess a number
+# myNumber = 9
+
+myNumber=random.randint(1,10)
+print("###############################################################")
+print("#                                                             #")
+print("#                       Guess a number                        #")
+print("#                                                             #")
+print("###############################################################")
+GameOn=True
+while(GameOn):
+    UserGuess=int(input("guess a number from 1-10 "))
+    if myNumber ==UserGuess:
+        print("You guessed it!")
+        GameOn=False
+    else:
+        print("you got it wrong :(")
+print("The number to guess was "+ str(myNumber))
