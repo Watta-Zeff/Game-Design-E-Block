2035144b546c19b951b5cc63110095101ef81069
Watta-Zeff& C:/Users/sadlerz24/AppData/Local/Microsoft/WindowsApps/python3.10.exe c:/Users/sadlerz24/.vscode/Game-Design-E-Block/AverageCode.py <sadlerz24@greenhill.org>
2022-01-29 19:53:27 -0600

    :(

--- TestUserInput.py
+++ TestUserInput.py
@@ -18,6 +18,7 @@ endChoice=''
 
 
 def endmenu():
+
     print("###############################################################")
     print("#                                                             #")
     print("#                                                             #")
@@ -42,68 +43,62 @@ def menu():
     print("#                      Choose a difficulty                    #")
     print("###############################################################")
 
-def randomnumgame():
-    menu()
 
-    check=True
-    while check:
-        try:
-            userChoice=(input("choice: "))
-            if userChoice =='a' or userChoice=='b' or userChoice =='c':
-                check = False
-        except ValueError:
-            print("Sorry, wrong choice, please enter a number 1 to 3 only >:(")
-            break
+menu()
 
-    # if choice>0 and choice <4
-    #Check=False
-        while(GameMenu):
+check=True
+while check:
+    try:
+        userChoice=(input("choice: "))
+        if userChoice =='a' or userChoice=='b' or userChoice =='c':
+            check = False
+    except ValueError:
+        print("Sorry, wrong choice, please enter a number 1 to 3 only >:(")
+        break
 
-            if userChoice =='a':
-                usernum = int(input("guess a number from 1-10 "))
-                if usernum == myNumber:
-                    print("You guessed it!")
-                    time.sleep(5)
-                    break
-                    
-                else:
-                    print("you got it wrong :(")
-                    print("The number to guess was "+ str(myNumber))
-                    time.sleep(5)
-                    break
+# if choice>0 and choice <4
+#Check=False
+    while(GameMenu):
 
-            if userChoice =='b':
-                usernum = int(input("guess a number from 1-50 "))
-                if usernum == myNumber2:
-                    print("You guessed it!")
-                    time.sleep(5)
-                    break
+        if userChoice =='a':
+            usernum = int(input("guess a number from 1-10 "))
+            if usernum == myNumber:
+                print("You guessed it!")
+                time.sleep(5)
+                break
+                
+            else:
+                print("you got it wrong :(")
+                print("The number to guess was "+ str(myNumber))
+                time.sleep(5)
+                break
 
-                else:
-                    print("you got it wrong :(")
-                    print("The number to guess was "+ str(myNumber2))
-                    time.sleep(5)
-                    break
-            if userChoice =='c':
-                usernum = int(input("guess a number from 1-100 "))
-                if myNumber3 ==userChoice:
-                    print("You guessed it!")
-                    time.sleep(5)
-                    break
+        if userChoice =='b':
+            usernum = int(input("guess a number from 1-50 "))
+            if usernum == myNumber2:
+                print("You guessed it!")
+                time.sleep(5)
+                break
 
-                else:
-                    print("you got it wrong :(")
-                    print("The number to guess was "+ str(myNumber3))
-                    time.sleep(5)
-                    break
+            else:
+                print("you got it wrong :(")
+                print("The number to guess was "+ str(myNumber2))
+                time.sleep(5)
+                break
+        if userChoice =='c':
+            usernum = int(input("guess a number from 1-100 "))
+            if myNumber3 ==userChoice:
+                print("You guessed it!")
+                time.sleep(5)
+                break
+
+            else:
+                print("you got it wrong :(")
+                print("The number to guess was "+ str(myNumber3))
+                time.sleep(5)
+                break
 
 os.system('cls')
 
 endmenu()
-while endChoice : (input(" "))
 
-if endChoice =='y':
-    randomnumgame()
-if endChoice =='n':
-    os.system('cls')
-    print("goodbye")
