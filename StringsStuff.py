51bb18f4c03c6f5add64720ebd380c194fb2993b
Watta-Zeff& C:/Users/sadlerz24/AppData/Local/Microsoft/WindowsApps/python3.10.exe c:/Users/sadlerz24/.vscode/Game-Design-E-Block/AverageCode.py <sadlerz24@greenhill.org>
2022-01-31 09:59:58 -0600

    Strings

---
+++ StringsStuff.py
@@ -0,0 +1,26 @@
+# #Zeffaniah Knight
+# 01/31/2022
+# strings array of characters
+# Has many functions
+import os
+
+os.system('cls')
+
+MyName= 'Zeffaniah Knight'
+
+# each letter or number has a block starting with 0
+
+MyStatement=""" My Name is so nice because
+blah blah blagggg
+
+what ever
+ever"""
+
+print (MyName[10])
+print (MyStatement)
+
+print ("My last name begins with " +MyName[6])
+
+if 'blah' in MyStatement:
+    print('true')
+print('expert' not in MyStatement)
