367eb6fcdade9698a477fe58cd412f0e26fa5ba7
Watta-Zeff <97113339+Watta-Zeff@users.noreply.github.com>
2022-03-23 09:57:18 -0500

    3/23/22

--- Learning Font.py
+++ Learning Font.py
@@ -1,18 +1,62 @@
-import pygame, time
 
+import pygame, time, random
+
+
+
+WIDTH=700
+HEIGHT=700
+wb=30
+hbox=30
+
+colors={'white':[255,255,255], 'red':[255,0,0], 'aqua':[102,153, 255],
+'orange':[255,85,0],'purple':[48,25,52],'navy':[5,31,64],'pink':[200,3,75], 'crimson':[168,26,26], 'gray':[96,96,96], 'blreen':[43,255,171] }
+
+background= colors.get('blreen')
+
+cr_color=colors.get('red')
+randColor=''
+def changeColor():
+    global randColor
+    colorCheck=True
+    while colorCheck:
+        randColor=random.choice(list(colors))
+        if colors.get(randColor) == background:
+            sq_color= colors.get(randColor)
+            
+        else:
+            colorCheck=False
+
+changeColor()
+sq_color= colors.get(randColor)
+    
 pygame.init()
-wind=pygame.display.set_mode((700,700))
-wind=pygame.display.set_caption("testing")
+wind=pygame.display.set_mode((WIDTH,HEIGHT))
+wind=pygame.display.set_caption("Circle Eats Square")
 #create different type of fonts
 
 TITLE_FNT=pygame.font.SysFont('comicsans', 80)
 MENU_FNT=pygame.font.SysFont('comicsans', 40)
 INST_FNT=pygame.font.SysFont('comicsans', 30)
-text=TITLE_FNT.render('Daddy', 1, (125,45,30))
+text=TITLE_FNT.render('He11', 1, (125,45,30))
 wind.fill((255,255,255))
 wind.blit(text,(50,50))
 text=INST_FNT.render("Write your instructions",1,(0,255,0))
 wind.blit(text,(220,220))
+
+wind.blit(text,(50,200))
+text=INST_FNT.render("---- W to Move Up ----")
+wind.blit(text,(50,150))
+text=INST_FNT.render("---- S to Move Down ----")
+wind.blit(text,(50,100))
+text=INST_FNT.render("---- A to Move Left ----")
+wind.blit(text,(50,50))
+text=INST_FNT.render("---- D to Move Right ----")
+pygame.display.update()
+
+pygame.time.delay(100000)
+
+
+
 pygame.display.update()
 
 pygame.time.delay(1000)
--- Square Devourer Game.py
+++ Square Devourer Game.py
@@ -39,30 +39,16 @@ square=pygame.Rect(xs,ys,wbox,hbox)
 
 #create screen
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption('Circle eats Square')
+pygame.display.set_caption('Circle Eats Square')
 
-#Menu / Instructions
-# pygame.init()
-# wind=pygame.display.set_mode((700,700))
-# wind=pygame.display.set_caption("Circle Eats Square Game")
-# #create different type of fonts
 
-# TITLE_FNT=pygame.font.SysFont('arial.ttf', 80)
-# MENU_FNT=pygame.font.SysFont('arial.ttf', 40)
-# INST_FNT=pygame.font.SysFont('arial.ttf', 30)
-# text=TITLE_FNT.render('Watta', 1, (125,45,30))
-# wind.fill((255,255,255))
-# wind.blit(text,(50,50))
-# text=INST_FNT.render("Write your instructions",1,(0,255,0))
-# wind.blit(text,(220,220))
-# pygame.display.update()
-
-# pygame.time.delay(1000)
 
 #define colors
 colors={'white':[255,255,255], 'red':[255,0,0], 'aqua':[102,153, 255],
-'orange':[255,85,0],'purple':[48,25,52],'navy':[5,31,64],'pink':[200,3,75], 'crimson':[168,26,26], 'gray':[96,96,96], 'blreen':[43,255,171] }
+'orange':[255,85,0],'purple':[48,25,52],'navy':[5,31,64],'pink':[200,3,75], 'crimson':[168,26,26], 'gray':[96,96,96], 'blreen':[43,255,171], 'black':[0,0,0] }
 
+#List for messages 
+MenuList=['Instructions', 'Settings', 'Level 1', 'Level 2', 'Level 3', 'Scoreboard', 'Exit']
 #Get colors
 background= colors.get('blreen')
 
@@ -73,7 +59,7 @@ def changeColor():
     colorCheck=True
     while colorCheck:
         randColor=random.choice(list(colors))
-        if randColor == background:
+        if colors.get(randColor) == background:
             sq_color= colors.get(randColor)
             
         else:
@@ -82,6 +68,81 @@ def changeColor():
 changeColor()
 sq_color= colors.get(randColor)
     
+
+
+
+
+
+
+#Menu / Instructions
+
+
+
+#create different type of fonts
+
+TITLE_FNT=pygame.font.SysFont('comicsans', 80)
+MENU_FNT=pygame.font.SysFont('arial.ttf', 40)
+INST_FNT=pygame.font.SysFont('arial.ttf', 40)
+
+text=TITLE_FNT.render('Menu', 1, (125,45,30))
+screen.fill((255,255,255))
+
+xt=WIDTH/2-text.get_width()/2
+screen.blit(text,(xt,50))
+#get the width of the text
+
+
+#Create First Button
+
+
+#Create square for menu
+xs=50
+ys=250
+wb=50
+hb=50
+
+menu_sq=colors.get('black')
+
+square=pygame.Rect(xs,ys,wbox,hbox)
+txty=250
+for i in range (7):
+    message=MenuList[i]
+    text=INST_FNT.render(message,1,(0,0,0))
+    screen.blit(text,(90,txty))
+    pygame.draw.rect(screen, menu_sq, square )
+    square.y +=50
+    txty+=50
+
+
+
+
+pygame.display.update()
+
+pygame.time.delay(100000)
+
+
+
+
+
+
+
+
+
+
+
+
+# text=INST_FNT.render("---- W to Move Up ----")
+# screen.blit(text,(50,250))
+# text=INST_FNT.render("---- S to Move Down ----")
+# screen.blit(text,(50,150))
+# text=INST_FNT.render("---- A to Move Left ----")
+# screen.blit(text,(50,100))
+# text=INST_FNT.render("---- D to Move Right ----")
+
+
+
+
+
 
 #Jump Code
 
